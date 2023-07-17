5556162b5afe864cdda6b88750d1a336310164f5
Ethan <sovernfinance@gmail.com>
2023-07-17 15:26:10 -0500

    Changing style/format for new dapp

--- src/layout/CustomFooter/CustomFooter.tsx
+++ src/layout/CustomFooter/CustomFooter.tsx
@@ -20,11 +20,7 @@ const styles = {
 const CustomFooter: FC = () => {
   return (
     <Footer style={styles.footer}>
-      Please, leave a ⭐️ on this{" "}
-      <a href="https://github.com/Pedrojok01/Web3-Boilerplate" target="_blank" rel="noopener noreferrer">
-        boilerplate
-      </a>{" "}
-      if you like it!
+
     </Footer>
   );
 };
