a6fcc8e69304fc3bf69238a96b59d66bbc5b98a0
Ethan <sovernfinance@gmail.com>
2023-08-25 15:29:08 -0500

    Added AcornVault.sol

---
+++ src/contracts/AcornVault.sol
@@ -0,0 +1,109 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract AcornVault is ERC20("a", "a"), ReentrancyGuard, Ownable {
+  using SafeERC20 for IERC20;
+  using EnumerableSet for EnumerableSet.AddressSet;
+  using SafeMath for uint256;
+  using Address for address payable;
+
+  IERC20 public stakedGlp;
+  uint256 public depositFee = 1; // 0.1%
+  uint256 public withdrawFee = 1; // 0.1%
+  address payable public feeReceiver;
+
+  EnumerableSet.AddressSet private holders;
+
+  uint256 public totalYield; // Track the total yield distributed
+  mapping(address => uint256) public lastDistributedYield; // Last distributed yield per holder
+
+  constructor(IERC20 _stakedGlp, address payable _feeReceiver) {
+    require(_feeReceiver != address(0), "Fee receiver cannot be zero address");
+    stakedGlp = _stakedGlp;
+    feeReceiver = _feeReceiver;
+  }
+
+  //Allow users to deposit tokens into the vault
+  function deposit(uint256 glpAmount) external nonReentrant {
+    require(glpAmount > 0, "Amount must be greater than 0");
+    uint256 fee = glpAmount.mul(depositFee).div(1000);
+    uint256 amountAfterFee = glpAmount.sub(fee);
+    stakedGlp.safeTransferFrom(msg.sender, address(this), glpAmount);
+    stakedGlp.safeTransfer(feeReceiver, fee);
+
+    _mint(msg.sender, amountAfterFee);
+    holders.add(msg.sender);
+    emit Deposit(msg.sender, glpAmount);
+  }
+  //Allow the treasury to deposit tokens into the vault for liquidity
+  function depositOwner(uint256 glpAmount) external onlyOwner {
+    require(glpAmount > 0, "Amount must be greater than 0");
+    stakedGlp.safeTransferFrom(msg.sender, address(this), glpAmount);
+    emit OwnerDeposit(msg.sender, glpAmount);
+  }
+
+  //Allow holders to withdraw thier tokens and yield
+  function withdraw(uint256 aGlpAmount) public nonReentrant {
+    require(balanceOf(msg.sender) >= aGlpAmount, "Not enough aGLP balance");
+    uint256 holderYield = (totalYield.sub(lastDistributedYield[msg.sender]))
+                                .mul(balanceOf(msg.sender))
+                                .div(totalSupply());
+    lastDistributedYield[msg.sender] = totalYield;
+    uint256 glpToReturn = aGlpAmount
+                                .mul(stakedGlp.balanceOf(address(this)).sub(totalYield))
+                                .div(totalSupply());
+    uint256 fee = glpToReturn.mul(withdrawFee).div(1000);
+    uint256 amountAfterFee = glpToReturn.sub(fee).add(holderYield);
+    _burn(msg.sender, aGlpAmount);
+    require(stakedGlp.balanceOf(address(this)) >= amountAfterFee, "Not enough GLP in contract");
+    stakedGlp.transfer(msg.sender, amountAfterFee);
+    stakedGlp.transfer(feeReceiver, fee);
+
+    if (balanceOf(msg.sender) == 0) {
+        holders.remove(msg.sender);
+    }
+
+    emit Withdraw(msg.sender, aGlpAmount);
+  }
+
+  // Allow the treasury to withdraw tokens for yield strategies
+  function withdrawOwner(uint256 percentage) external onlyOwner {
+    require(percentage > 0 && percentage <= 50, "Percentage must be between 1 and 50");
+    uint256 amountToWithdraw = stakedGlp.balanceOf(address(this)).mul(percentage).div(100);
+    stakedGlp.transfer(owner(), amountToWithdraw);
+  }
+
+  //Distribute yield to holders
+  function distributeAGLP(uint256 glpAmount) external onlyOwner {
+    require(glpAmount > 0, "Amount must be greater than 0");
+    stakedGlp.safeTransferFrom(msg.sender, address(this), glpAmount);
+    totalYield = totalYield.add(glpAmount);
+    emit Distribution(msg.sender, glpAmount);
+  }
+
+  //Treasury management fee
+  function setFeeReceiver(address payable _feeReceiver) external onlyOwner {
+    require(_feeReceiver != address(0), "Fee receiver cannot be zero address");
+    feeReceiver = _feeReceiver;
+    emit FeeReceiverUpdated(_feeReceiver);
+  }
+
+  event Deposit(address indexed user, uint256 amount);
+  event OwnerDeposit(address indexed owner, uint256 amount);
+  event Withdraw(address indexed user, uint256 amount);
+  event FeeReceiverUpdated(address newFeeReceiver);
+  event Distribution(address indexed owner, uint256 amount);
+}
+
+
+
+
+
