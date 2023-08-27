abccd5b973ed2c1259a193ac02ad2841066dafc0
Ethan <sovernfinance@gmail.com>
2023-08-27 01:55:06 -0500

    Vault UI updates

--- src/components/displayPane/components/Vault.tsx
+++ src/components/displayPane/components/Vault.tsx
@@ -33,6 +33,8 @@ const Vault: FC<VaultProps> = ({ vault }) => {
   const [depositAmount, setDepositAmount] = useState('');
   const [withdrawAmount, setWithdrawAmount] = useState('');
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [showStrategyInfo, setShowStrategyInfo] = useState(false);
+  const [showActions, setShowActions] = useState(false); // Add this line to manage visibility
   const [depositSuccessMessage, setDepositSuccessMessage] = useState<string | null>(null);
   const [withdrawSuccessMessage, setWithdrawSuccessMessage] = useState<string | null>(null);
   const { account: accountFromWeb3, provider: providerFromWeb3 } = useWeb3React();
@@ -174,283 +176,306 @@ const Vault: FC<VaultProps> = ({ vault }) => {
 
   const handleModalToggle = () => {
     setIsModalVisible(!isModalVisible);
+    setShowStrategyInfo(!showStrategyInfo);
+  };
+
+  const toggleActions = () => {
+    setShowActions(!showActions); // Toggle the state
   };
 
 
   return (
-    <div
-      style={{
-        width: "100%",
-        marginBottom: "30px",
-        marginTop: "15px",
-        display: 'flex',
-        justifyContent: 'center',
-        transition: 'transform .2s, border-color .2s',
-      }}
-      onMouseOver={(e) => {
-        const target = e.currentTarget;
-        target.style.transform = 'scale(1.02)';
-        const card = target.querySelector('.ant-card') as HTMLElement;
-        if (card) card.style.borderColor = '#064576';
-      }}
-      onMouseOut={(e) => {
-        const target = e.currentTarget;
-        target.style.transform = 'scale(1)';
-        const card = target.querySelector('.ant-card') as HTMLElement;
-        if (card) card.style.borderColor = '#064576';
-      }}
-    >
-      {depositSuccessMessage && (
-        <Modal
-          title="Transaction Successful"
-          visible={!!depositSuccessMessage}
-          onCancel={() => setDepositSuccessMessage(null)}
-          footer={null}
-          centered
-          bodyStyle={{ backgroundColor: "transparent", color: "white" }}
-          wrapClassName="custom-modal"
-        >
-          <Card
-            style={{
-              backgroundColor: "#011F37",
-              color: "white",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "space-between",
-              alignItems: "center",
-              width: "auto",
-              minHeight: "10vh",
-              marginTop: "20px",
-              border: "transparent"
-            }}
-          >
-            <p style={{ fontSize: "20px" }}>{depositSuccessMessage}</p>
-          </Card>
-        </Modal>
-      )}
-      {withdrawSuccessMessage && (
-        <Modal
-          title="Transaction Successful"
-          visible={!!withdrawSuccessMessage}
-          onCancel={() => setWithdrawSuccessMessage(null)}
-          footer={null}
-          centered
-          bodyStyle={{ backgroundColor: "transparent", color: "white" }}
-          wrapClassName="custom-modal"
-        >
-          <Card
-            style={{
-              backgroundColor: "#011F37",
-              color: "white",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "space-between",
-              alignItems: "center",
-              width: "auto",
-              minHeight: "10vh",
-              marginTop: "20px",
-              border: "transparent"
-            }}
-          >
-            <p style={{ fontSize: "20px" }}>{withdrawSuccessMessage}</p>
-          </Card>
-        </Modal>
-      )}
-      {errorMessage && (
-        <Modal
-          title="Transaction Failed"
-          visible={errorMessage ? true : false}
-          onCancel={() => setErrorMessage(null)}
-          footer={null}
-          centered
-          bodyStyle={{ backgroundColor: "transparent", color: "white" }}
-          wrapClassName="custom-modal"
-        >
-          <Card
-            style={{
-              backgroundColor: "#011F37",
-              color: "white",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "space-between",
-              alignItems: "center",
-              width: "auto",
-              minHeight: "10vh",
-              marginTop: "20px",
-              border: "transparent"
-            }}
-          >
-            <p style={{ fontSize: "20px" }}>{errorMessage}</p>
-          </Card>
-        </Modal>
-      )}
-      <Card
-  style={{
-    backgroundColor: 'transparent',
-    color: 'white',
-    borderRadius: '12px',
-    border: '2px solid #064576',
-    transition: 'border-color .2s',
-    maxWidth: "100%",
-    width: '100%',
-  }}
->
-  <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-    <div style={{ display: 'flex', alignItems: 'center' }}>
-      <img src={vault.logo} alt={`${vault.name} Logo`} width={isMobile ? '45px' : '60px'} />
-      <h2 style={{ marginLeft: '20px' }}>{vault.name}</h2>
-    </div>
-    <Button
-      onClick={handleModalToggle}
-      style={{
-        color: 'white',
-        backgroundColor: '#011F37',  // Add this line
-        border: '1px solid #011F37',
-        borderRadius: '12px',
-      }}
-    >
-      Strategy Info
-    </Button>
-  </div>
-  <Divider style={{ borderColor: '#064576', borderWidth: '2px', marginTop: '20px', marginBottom: '20px' }} />
-  <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-      <div>
-      <h3>Vault Balance:</h3>
-      <h2>{vaultTokenBalance.isZero() ? "0.0" : parseFloat(ethers.utils.formatUnits(vaultTokenBalance, decimals)).toFixed(2)}</h2>
-    </div>
-    <div>
-      <h3>APR:</h3>
-      <h2>{vault.apr}%</h2>
-    </div>
-  </div>
-  <Divider style={{ borderColor: '#064576', borderWidth: '2px', marginTop: '20px', marginBottom: '20px' }} />
-  <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-  <div style={{ width: '48%' }}>
-    <Input
-      placeholder={`Balance: ${userBalance}`}
-      value={depositAmount}
-      onChange={(e) => setDepositAmount(e.target.value)}
-      style={{
-        color: 'white',  // Add this line
-        backgroundColor: '#011F37',  // Add this line
-        borderColor: '#011F37',  // Add this line
-      }}
-    />
-    <Button
-      onClick={() => setDepositAmount(userBalance)}
-      style={{
-        color: 'white',
-        backgroundColor: '#011F37',
-        border: '1px solid #011F37',
-        borderRadius: '12px',
-        marginTop: '10px',
-        width: '100%',  // Add this line
-      }}
-    >
-      MAX
-    </Button>
-
-    <Button
-      onClick={deposit}
-      style={{
-        color: 'white',
-        backgroundColor: '#011F37',  // Add this line
-        border: '1px solid #011F37',  // Change this line
-        borderRadius: '12px',
-        marginTop: '10px',
-        width: '100%',
-      }}
-    >
-      Deposit
-    </Button>
-  </div>
-
-    <div style={{ width: '48%' }}>
-    <Input
-      placeholder={`Vault balance: ${vaultBalanceFormatted}`}
-      value={withdrawAmount}
-      onChange={(e) => setWithdrawAmount(e.target.value)}
-      style={{
-        color: 'white',
-        backgroundColor: '#011F37',
-        borderColor: '#011F37',
-      }}
-    />
-    <Button
-      onClick={() => setWithdrawAmount(ethers.utils.formatUnits(vaultTokenBalance, decimals))}
-      // Here it sets the withdrawAmount to be the user's vaultTokenBalance
-      style={{
-        color: 'white',
-        backgroundColor: '#011F37',
-        border: '1px solid #011F37',
-        borderRadius: '12px',
-        marginTop: '10px',
-        width: '100%',
-      }}
-    >
-      MAX
-    </Button>
-    <Button
-      onClick={handleWithdraw}
-      style={{
-        color: 'white',
-        backgroundColor: '#011F37',
-        border: '1px solid #011F37',
-        borderRadius: '12px',
-        marginTop: '10px',
-        width: '100%',
-      }}
-    >
-      Withdraw
-    </Button>
-  </div>
-
-    </div>
-   </Card>
-
-
-      {isModalVisible && (
-        <Modal
-        title={
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <img src={vault.logo} alt={`${vault.name} Logo`} style={{ width: '24px', marginRight: '10px' }} />
-            {vault.name}
-          </div>
-        }
-        visible={isModalVisible}
-        onCancel={handleModalToggle}
+  <div
+    onClick={toggleActions}
+    style={{
+      width: "100%",
+      marginBottom: "30px",
+      marginTop: "15px",
+      display: 'flex',
+      justifyContent: 'center',
+      transition: 'transform .2s, border-color .2s',
+    }}
+    onMouseOver={(e) => {
+      const target = e.currentTarget;
+      target.style.transform = 'scale(1.02)';
+      const card = target.querySelector('.ant-card') as HTMLElement;
+      if (card) card.style.borderColor = '#064576';
+    }}
+    onMouseOut={(e) => {
+      const target = e.currentTarget;
+      target.style.transform = 'scale(1)';
+      const card = target.querySelector('.ant-card') as HTMLElement;
+      if (card) card.style.borderColor = '#064576';
+    }}
+  >
+    {depositSuccessMessage && (
+      <Modal
+        title="Transaction Successful"
+        visible={!!depositSuccessMessage}
+        onCancel={() => setDepositSuccessMessage(null)}
         footer={null}
         centered
-        bodyStyle={{ backgroundColor: "transparent", color: "transparent" }}
+        bodyStyle={{ backgroundColor: "transparent", color: "white" }}
         wrapClassName="custom-modal"
       >
-          <Card
-            style={{
-              backgroundColor: "#011F37",  // Change this line
-              color: "white",  // Change this line
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "space-between",
-              alignItems: "center",
-              width: "auto",
-              minHeight: "10vh",
-              marginTop: "20px",
-              border: "transparent"
-            }}
-          >
-            <p style={{ fontSize: "15px" }}>{vault.textAboveTitle}</p> {/* Text above the title */}
-            <Divider style={{ borderColor: '#064576', borderWidth: '2px', marginTop: '20px', marginBottom: '20px' }} />
-            <h2 style={{ fontSize: "17px" }}>Vault Strategy</h2>
-            <p style={{ fontSize: "17px" }}>{vault.strategy}</p>
-            <Divider style={{ borderColor: '#064576', borderWidth: '2px', marginTop: '20px', marginBottom: '20px' }} />
-            <div style={{ display: 'flex', justifyContent: 'space-between', fontSize: "15px" }}>
-              <p>{vault.textBelowDescription}</p> {/* Text below the description */}
-            </div>
-            {/* Other Modal Content */}
-          </Card>
-        </Modal>
-      )}
-    </div>
+        <Card
+          style={{
+            backgroundColor: "#011F37",
+            color: "white",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: "auto",
+            minHeight: "10vh",
+            marginTop: "20px",
+            border: "transparent"
+          }}
+        >
+          <p style={{ fontSize: "20px" }}>{depositSuccessMessage}</p>
+        </Card>
+      </Modal>
+    )}
+    {withdrawSuccessMessage && (
+      <Modal
+        title="Transaction Successful"
+        visible={!!withdrawSuccessMessage}
+        onCancel={() => setWithdrawSuccessMessage(null)}
+        footer={null}
+        centered
+        bodyStyle={{ backgroundColor: "transparent", color: "white" }}
+        wrapClassName="custom-modal"
+      >
+        <Card
+          style={{
+            backgroundColor: "#011F37",
+            color: "white",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: "auto",
+            minHeight: "10vh",
+            marginTop: "20px",
+            border: "transparent"
+          }}
+        >
+          <p style={{ fontSize: "20px" }}>{withdrawSuccessMessage}</p>
+        </Card>
+      </Modal>
+    )}
+    {errorMessage && (
+      <Modal
+        title="Transaction Failed"
+        visible={errorMessage ? true : false}
+        onCancel={() => setErrorMessage(null)}
+        footer={null}
+        centered
+        bodyStyle={{ backgroundColor: "transparent", color: "white" }}
+        wrapClassName="custom-modal"
+      >
+        <Card
+          style={{
+            backgroundColor: "#011F37",
+            color: "white",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: "auto",
+            minHeight: "10vh",
+            marginTop: "20px",
+            border: "transparent"
+          }}
+        >
+          <p style={{ fontSize: "20px" }}>{errorMessage}</p>
+        </Card>
+      </Modal>
+    )}
+    <Card
+      style={{
+      backgroundColor: 'transparent',
+      color: 'white',
+      borderRadius: '12px',
+      border: '2px solid #064576',
+      transition: 'border-color .2s',
+      maxWidth: "100%",
+      width: '100%',
+      }}
+      >
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <img src={vault.logo} alt={`${vault.name} Logo`} width={isMobile ? '45px' : '60px'} />
+        <h2 style={{ marginLeft: '20px' }}>{vault.name}</h2>
+      </div>
+      <Button
+        onClick={(e) => {
+          e.stopPropagation();
+          handleModalToggle();
+        }}
+        style={{
+          color: 'white',
+          backgroundColor: '#011F37',
+          border: '1px solid #011F37',
+          borderRadius: '12px',
+        }}
+      >
+        Strategy Info
+      </Button>
+      </div>
+      <Divider style={{ borderColor: '#064576', borderWidth: '2px', marginTop: '20px', marginBottom: '20px' }} />
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <div>
+        <h3>Vault Balance:</h3>
+        <h2>{vaultTokenBalance.isZero() ? "0.0" : parseFloat(ethers.utils.formatUnits(vaultTokenBalance, decimals)).toFixed(2)}</h2>
+      </div>
+      <div>
+        <h3>APR:</h3>
+        <h2>{vault.apr}%</h2>
+      </div>
+      </div>
+      <Divider style={{ borderColor: '#064576', borderWidth: '2px', marginTop: '20px', marginBottom: '20px' }} />
+      {showActions && !showStrategyInfo && (
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+      <div style={{ width: '48%' }}>
+      <Input
+        placeholder={`Balance: ${userBalance}`}
+        value={depositAmount}
+        onChange={(e) => setDepositAmount(e.target.value)}
+        onClick={(e) => e.stopPropagation()}
+        onMouseDown={(e) => e.stopPropagation()}
+        style={{
+          color: 'white',
+          backgroundColor: '#011F37',
+          borderColor: '#011F37',
+        }}
+      />
+      <Button
+        onClick={(e) => {
+          e.stopPropagation(); // Stop event from propagating to parent
+          setDepositAmount(userBalance);
+        }}
+        style={{
+          color: 'white',
+          backgroundColor: '#011F37',
+          border: '1px solid #011F37',
+          borderRadius: '12px',
+          marginTop: '10px',
+          width: '100%',  // Add this line
+        }}
+      >
+        MAX
+      </Button>
+
+      <Button
+        onClick={(e) => {
+          e.stopPropagation(); // Stop event from propagating to parent
+          deposit();
+        }}
+        style={{
+          color: 'white',
+          backgroundColor: '#011F37',  // Add this line
+          border: '1px solid #011F37',  // Change this line
+          borderRadius: '12px',
+          marginTop: '10px',
+          width: '100%',
+        }}
+      >
+        Deposit
+      </Button>
+      </div>
+
+      <div style={{ width: '48%' }}>
+      <Input
+        placeholder={`Vault balance: ${vaultBalanceFormatted}`}
+        value={withdrawAmount}
+        onChange={(e) => setWithdrawAmount(e.target.value)}
+        onClick={(e) => e.stopPropagation()}
+        onMouseDown={(e) => e.stopPropagation()}
+        style={{
+          color: 'white',
+          backgroundColor: '#011F37',
+          borderColor: '#011F37',
+        }}
+      />
+      <Button
+        onClick={(e) => {
+          e.stopPropagation(); // Stop event from propagating to parent
+          setWithdrawAmount(ethers.utils.formatUnits(vaultTokenBalance, decimals));
+        }}
+        style={{
+          color: 'white',
+          backgroundColor: '#011F37',
+          border: '1px solid #011F37',
+          borderRadius: '12px',
+          marginTop: '10px',
+          width: '100%',
+        }}
+      >
+        MAX
+      </Button>
+      <Button
+        onClick={(e) => {
+          e.stopPropagation(); // Stop event from propagating to parent
+          handleWithdraw();
+        }}
+        style={{
+          color: 'white',
+          backgroundColor: '#011F37',
+          border: '1px solid #011F37',
+          borderRadius: '12px',
+          marginTop: '10px',
+          width: '100%',
+        }}
+      >
+        Withdraw
+      </Button>
+      </div>
+      </div>
+    )}
+  </Card>
+
+    {isModalVisible && (
+      <Modal
+      title={
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <img src={vault.logo} alt={`${vault.name} Logo`} style={{ width: '24px', marginRight: '10px' }} />
+          {vault.name}
+        </div>
+      }
+      visible={isModalVisible}
+      onCancel={handleModalToggle}
+      footer={null}
+      centered
+      bodyStyle={{ backgroundColor: "transparent", color: "transparent" }}
+      wrapClassName="custom-modal"
+    >
+        <Card
+          style={{
+            backgroundColor: "#011F37",  // Change this line
+            color: "white",  // Change this line
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: "auto",
+            minHeight: "10vh",
+            marginTop: "20px",
+            border: "transparent"
+          }}
+        >
+          <p style={{ fontSize: "15px" }}>{vault.textAboveTitle}</p>
+          <Divider style={{ borderColor: '#064576', borderWidth: '2px', marginTop: '20px', marginBottom: '20px' }} />
+          <h2 style={{ fontSize: "17px" }}>Vault Strategy</h2>
+          <p style={{ fontSize: "17px" }}>{vault.strategy}</p>
+          <Divider style={{ borderColor: '#064576', borderWidth: '2px', marginTop: '20px', marginBottom: '20px' }} />
+          <div style={{ display: 'flex', justifyContent: 'space-between', fontSize: "15px" }}>
+            <p>{vault.textBelowDescription}</p>
+          </div>
+        </Card>
+      </Modal>
+    )}
+  </div>
   );
 };
 
